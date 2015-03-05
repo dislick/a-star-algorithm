38abfc053c712b197de0f3d2974a7118cd061d27
Patrick Muff <muff.pa@gmail.com>
2015-03-05 18:27:13 +0100

    Basic algorithm implementation

--- astar.js
+++ astar.js
@@ -1,5 +1,6 @@
 (function() {
 
+  // generate a random two-dimensional map array
   var map = (function(width, height, wallDensity) {
     var _map = [];
     for (var x = 0; x < width; x++) {
@@ -12,6 +13,46 @@
     }
     return _map;
   })(50, 50, 7);
+
+
+
+
+  /**
+   * Executes an A* search alogrithm 
+   * @param  {Node} start 
+   * @param  {Node} end   
+   * @return {[Node]} Returns an array with the path nodes
+   */
+  var calculatePath = function(map, startNode, endNode) {
+    // this array holds the next nodes to process
+    var openList = [];
+    // and this one holds all the nodes that have already been processed
+    var closedList = [];
+
+    // push the first node into the array to start the processing loop
+    openList.push(startNode);
+
+    // execute this block while there are still nodes that are open
+    while (openList.length > 0) {
+      // get and remove the node with the smallest F value
+      var currentNode = _.min(openList, function(node) { return node.f });
+      openList = _.without(openList, currentNode);
+
+      // check if endNode has been found and end the algorithm
+      if (currentNode.isEqual(endNode)) {
+        return true;
+      }
+
+      // find neighbors etc
+         
+
+      // add node to closed list and move on
+      closedList.push(currentNode);
+    }
+
+  };
+
+
 
   var canvas = document.querySelector('canvas');
   var context = canvas.getContext('2d');
--- astar.node.js
+++ astar.node.js
@@ -6,6 +6,48 @@
     this.type = type;
   };
 
+  Node.prototype.isEqual = function(node) {
+    return (this.x == node.x && this.y = node.y);
+  };
+
+  Node.prototype.getNeighbors = function(map) {
+    var neighbors = [];
+    // shortcuts to coordinates as they are used often below
+    var x = this.x;
+    var y = this.y;
+   
+    /*
+      | 0 | 1  | 2  | 3  | 4 |
+      | 1 |    | #1 |    |   |
+      | 2 | #4 | X  | #2 |   |
+      | 3 |    | #3 |    |   |
+      | 4 |    |    |    |   |
+    */
+
+    // #4 left
+    if (map[x - 1] && map[x - 1][y]) 
+    {
+      neighbors.push(map[x - 1][y]);
+    }
+    // #2 right
+    if (map[x + 1] && map[x + 1][y]) 
+    {
+      neighbors.push(map[x + 1][y]);
+    }
+    // #1 top
+    if (map[x] && map[x][y - 1])
+    {
+      neighbors.push(map[x][y - 1]);
+    }
+    // #3 bottom
+    if (map[x] && map[x][y + 1])
+    {
+      neighbors.push(map[x][y + 1]);
+    }
+
+    return neighbors;
+  };
+
   this.Node = Node;
 
 })(this);
